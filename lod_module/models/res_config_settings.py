87681d78dcd7d6038dc0971c5689e80760161df3
rcid <rcidb31@gmail.com>
2026-02-17 12:07:36 -0300

    fix: corrige sintaxis en res_config_settings.py

--- lod_module/models/res_config_settings.py
+++ lod_module/models/res_config_settings.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from odoo import fields, models. # odoo es el framework que usamos
+from odoo import fields, models 
 
 
 class ResConfigSettings(models.TransientModel): # heredamos de res.config.settings
